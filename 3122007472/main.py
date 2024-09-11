e4f318a168ba96d6ca0cebf83959049b00f275ab
LAObeeDENG <1132470912@qq.com>
2024-09-11 21:30:01 +0800

    基于jieba分词和余弦相似度的查重（粗略版） 新增了一个similarity（text1, text2）函数，封装了之前的函数，可以直接通过读取路径来计算文本的查重

--- 3122007472/main.py
+++ 3122007472/main.py
@@ -15,12 +15,12 @@ def preprocess_text(text):
     # 去除标点符号
     text = re.sub(r'[^\w\s]', '', text)
     # 去除多余的空白符
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r'\s+', '', text).strip()
     return text
 
 # 使用Jieba对文本进行分词
 def segment_text(text):
-    words = jieba.lcut(text)
+    words = jieba.lcut(text,cut_all=True)
     return words
 
 # 计算词频
@@ -40,6 +40,7 @@ def text_to_vector(words, vocabulary):
     return vector
 
 
+#计算余弦相似度
 def calculate_cosine_similarity(vec1, vec2):
     vec1 = [float(i) for i in vec1]
     vec2 = [float(i) for i in vec2]
@@ -54,35 +55,34 @@ def calculate_cosine_similarity(vec1, vec2):
     return dot_product / (magnitude1 * magnitude2)
 
 
+# 完整的余弦查重计算
+def similarity(text1, text2):
+    r_text1=read_file(text1)
+    r_text2=read_file(text2)
 
+    # 预处理
+    p_text1 = preprocess_text(r_text1)
+    p_text2 = preprocess_text(r_text2)
 
-def main():
-    original_file = "../examples/orig.txt"  # 获取命令行传入的原文文件路径
-    modified_file = "../examples/orig_0.8_del.txt"  # 获取命令行传入的抄袭版文件路径
-    output_file = "../examples/output.txt"  # 获取命令行传入的输出文件路径
+    # jieba分词
+    original_words = segment_text(p_text1)
+    modified_words = segment_text(p_text2)
 
-    # 读取文件并预处理
-    original_text = preprocess_text(read_file(original_file))
-    modified_text = preprocess_text(read_file(modified_file))
-
-    # Jieba 分词
-    original_words = segment_text(original_text)
-    modified_words = segment_text(modified_text)
-
-    # 构建词汇表（词汇表基于两个文本的所有词）
+    # 字典
     vocabulary = list(set(original_words + modified_words))
-
+    print(vocabulary)
     # 将两个文本转化为词频向量
     original_vector = text_to_vector(original_words, vocabulary)
     modified_vector = text_to_vector(modified_words, vocabulary)
-
+    print(original_vector)
+    print(modified_vector)
     # 计算余弦相似度
-    similarity = calculate_cosine_similarity(original_vector, modified_vector)
+    similarity_result = calculate_cosine_similarity(original_vector, modified_vector)
+    return similarity_result
 
-    # 输出相似度结果
-    # with open(output_file, 'w', encoding='utf-8') as file:
-    print(f"文本查重：{similarity:.2f}")
-        # file.write(f"{similarity:.2f}\n")
 
 if __name__ == "__main__":
-    main()
+    text1 = "../examples/orig.txt"
+    text2 = "../examples/orig_0.8_del.txt"
+    similarity_result = similarity(text1, text2)
+    print(f"文本查重：{similarity_result:.20f}")
